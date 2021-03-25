f82b5d2cb93e1e5d9f49a71c2a93dea97642b45e
Rob Cameron <rob.cameron@fastmail.com>
2021-03-24 17:26:39 -0700

    Update Chapter 1 example to use refactored Point/Vector

--- examples/chapter1/run.rb
+++ examples/chapter1/run.rb
@@ -1,6 +1,23 @@
-require_relative '../../tuple'
-require_relative './projectile'
-require_relative './environment'
+require_relative '../../point'
+require_relative '../../vector'
+
+class Projectile
+  attr_reader :position, :velocity
+
+  def initialize(position, velocity)
+    @position = position
+    @velocity = velocity
+  end
+end
+
+class Environment
+  attr_reader :gravity, :wind
+
+  def initialize(gravity, wind)
+    @gravity = gravity
+    @wind = wind
+  end
+end
 
 def tick(environment, projectile)
   position = projectile.position + projectile.velocity
@@ -8,8 +25,8 @@ def tick(environment, projectile)
   Projectile.new(position, velocity)
 end
 
-projectile = Projectile.new(Tuple.new(0, 1, 0, :point), Tuple.new(1, 1, 0, :vector).normalize)
-environment = Environment.new(Tuple.new(0, -0.1, 0, :vector), Tuple.new(-0.01, 0, 0, :vector))
+projectile = Projectile.new(Point.new(0, 1, 0), Vector.new(1, 1, 0).normalize)
+environment = Environment.new(Vector.new(0, -0.1, 0), Vector.new(-0.01, 0, 0))
 count = 0
 
 while(projectile.position.y > 0) do
