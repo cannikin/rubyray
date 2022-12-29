c21fb4ee186562cd4c83333d99821dee21647851
Rob Cameron <cannikin@fastmail.com>
2022-12-29 15:37:08 -0800

    Move PointLight properties into Light

--- lib/light.rb
+++ lib/light.rb
@@ -1,3 +1,10 @@
 class Light
 
+  attr_reader :position, :intensity
+
+  def initialize(position, intensity)
+    @position = position
+    @intensity = intensity
+  end
+
 end
--- lib/point_light.rb
+++ lib/point_light.rb
@@ -2,11 +2,4 @@ require_relative './light'
 
 class PointLight < Light
 
-  attr_reader :position, :intensity
-
-  def initialize(position, intensity)
-    @position = position
-    @intensity = intensity
-  end
-
 end
